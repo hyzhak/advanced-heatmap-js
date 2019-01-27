e02542981b8ebffdd94d869b0246f25b2322b9a4
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-01-28 00:57:51 +0100

    setup webpack dev server

--- example/hello-world/webpack.config.js
+++ example/hello-world/webpack.config.js
@@ -1,8 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
+
+  target: 'web',
 
   entry: './src/index.js',
 
@@ -12,8 +15,23 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: true
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+
     new HtmlWebpackPlugin({
       title: 'Hello advanced Heatmap'
     })
-  ]
+  ],
+
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    hot: true,
+    port: 7777
+  }
 };
