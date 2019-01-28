23e6bdc0f8d9efbca0f8a181375e5e0a4e954d08
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-01-28 01:18:59 +0100

    just experiment with canvas

--- example/hello-world/src/index.js
+++ example/hello-world/src/index.js
@@ -1,7 +1,24 @@
-function component() {
-  let element = document.createElement('div');
-  element.innerHTML = ['Hello', 'advanced', 'heatmap'].join(' ');
-  return element;
+import HeatMap from '../../..'
+
+function createHeatmap () {
+  // let element = document.createElement('div');
+  // element.innerHTML = ['Hello', 'advanced', 'heatmap'].join(' ');
+
+  const canvas = document.createElement('canvas');
+
+  const hp = new HeatMap({
+    canvas
+  });
+
+  const numOfPoints = 100;
+
+  for (let i = 0; i < numOfPoints; i++) {
+    hp.add([256 * Math.random(), 256 * Math.random(), 10 * Math.random()]);
+  }
+
+  hp.render();
+
+  return canvas;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(createHeatmap());
