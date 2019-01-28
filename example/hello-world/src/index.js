3505d3a4d7956af37ef6c5395e75f4577e83ec3a
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-01-28 01:26:16 +0100

    simulate brownian motion

--- example/hello-world/src/index.js
+++ example/hello-world/src/index.js
@@ -1,19 +1,37 @@
 import HeatMap from '../../..'
 
 function createHeatmap () {
-  // let element = document.createElement('div');
-  // element.innerHTML = ['Hello', 'advanced', 'heatmap'].join(' ');
-
+  const width = 256;
+  const height = 256;
   const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
 
   const hp = new HeatMap({
     canvas
   });
 
-  const numOfPoints = 100;
+  const numOfPoints = 1000;
 
+  let x = width / 2;
+  let y = height / 2;
+
+  // Brownian (random) motion
+  const step = 10;
   for (let i = 0; i < numOfPoints; i++) {
-    hp.add([256 * Math.random(), 256 * Math.random(), 10 * Math.random()]);
+    x += step * Math.random() - 5;
+    y += step * Math.random() - 5;
+    if (x > width) {
+      x -= width;
+    } else if (x < 0) {
+      x += width;
+    }
+    if (y > width) {
+      y -= width;
+    } else if (y < 0) {
+      y += width;
+    }
+    hp.add([x, y, 10 * Math.random()]);
   }
 
   hp.render();
